5d48baefd6c5d6e7bfef495dd788face476b8b1e
vcuenca <vcuenca.1989@gmail.com>
2024-10-18 09:01:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,request, jsonify, render_template
 
 app = Flask(__name__)
@@ -6,3 +7,24 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+API_KEY = os.environ['SECRETO']
+
+# Función para verificar la clave API
+def verify_api_key(api_key):
+    if api_key != API_KEY:
+        return jsonify({'message': 'Clave API inválida'}), 401
+
+# Endpoint protegido que requiere la clave API
+@app.route('/items', methods=['POST'])
+def read_items():
+    # Verifica la clave API en la cabecera de la solicitud
+    api_key = request.headers.get('api_key')
+    if not api_key:
+        return jsonify({'message': 'Falta la clave API'}), 401
+
+    response = verify_api_key(api_key)
+    if response:
+        return response
+
+    return jsonify({'items': ['Item 1', 'Item 2']})
